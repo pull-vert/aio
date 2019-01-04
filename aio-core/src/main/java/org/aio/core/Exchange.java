7ce153697649a4f55f2669f91cb8ea17b5c914a9
pull-vert <frederic.montariol@gmail.com>
2019-01-04 11:25:08 +0100

    update copyright header (2019)

--- aio-core/src/main/java/org/aio/core/Exchange.java
+++ aio-core/src/main/java/org/aio/core/Exchange.java
@@ -1,5 +1,5 @@
 ///*
-// * Copyright (c) 2018 AIO's author : Frédéric Montariol
+// * Copyright (c) 2018-2019 AIO's author : Frédéric Montariol
 // *
 // * Use of this source code is governed by the GNU General Public License v2.0,
 // * and is subject to the "Classpath" exception as provided in the LICENSE
